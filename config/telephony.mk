d2fe7d081cb1c3f129a11ba29a321e608851fcc8
Luke Yue <lukedyue@gmail.com>
2019-02-23 19:28:26 +0800

    rr: Derp
    Change-Id: I83a5745b640cad10f8c5a63df2cd775dee568213
    Signed-off-by: Luke Yue <lukedyue@gmail.com>

--- config/telephony.mk
+++ config/telephony.mk
@@ -6,6 +6,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     apns-conf.xml
 
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/rr/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     messaging \
