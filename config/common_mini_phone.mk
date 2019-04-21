a19f50a08de626d132770b89d70739ae155867e7
Luke Yue <lukedyue@gmail.com>
2019-04-21 15:34:06 +0800

    rr: Switch to Gboard.
    Change-Id: Iffec40b5e06a489ff64b5fa5528c40d5a4e1e233
    Signed-off-by: Luke Yue <lukedyue@gmail.com>

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -1,8 +1,4 @@
 # Inherit mini common Lineage stuff
 $(call inherit-product, vendor/rr/config/common_mini.mk)
 
-# Required packages
-PRODUCT_PACKAGES += \
-    LatinIME
-
 $(call inherit-product, vendor/rr/config/telephony.mk)
--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -1,6 +1,3 @@
 # Inherit mini common Lineage stuff
 $(call inherit-product, vendor/rr/config/common_mini.mk)
 
-# Required packages
-PRODUCT_PACKAGES += \
-    LatinIME
